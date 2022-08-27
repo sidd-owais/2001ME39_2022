aec9bb5024e7c1bd7f628a16d4cd5d6eb2e9a4fa
sidd_owais <75408212+sidd-owais@users.noreply.github.com>
2022-08-27 20:17:38 +0530

    final ans

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -2,3 +2,9 @@ def factorial(n):
     if(n == 1 or n == 0):
         return 1
     return (n*factorial(n-1))
+
+number = int(input("Enter a number whose need to be calculated:"))
+if(number < 0):
+    print("Factorial doesn't exist")
+else:
+    print("Factorial of ",number, " is ",factorial(number))
