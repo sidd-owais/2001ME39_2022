0b105d0eae8d4e03dc50fad7357c9936e7b58d57
sidd_owais <75408212+sidd-owais@users.noreply.github.com>
2022-09-14 17:37:26 +0530

    reading file

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,10 +1,12 @@
-def factorial(n):
-    if(n == 1 or n == 0):
-        return 1
-    return (n*factorial(n-1))
+import csv
 
-number = int(input("Enter a number whose need to be calculated:"))
-if(number < 0):
-    print("Factorial doesn't exist")
-else:
-    print("Factorial of ",number, " is ",factorial(number))
+import math
+
+octant = [0 for i in range(9)]
+
+# opening octant_input file for reading purpose
+
+data = list(csv.reader(open('octant_input.csv','r'))) 
+
+csv.writer(open('octant_output.csv','w',newline='')).writerows(data)
+
