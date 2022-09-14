f6c6e1138460135c04be8bd072e36affe6652e1c
sidd_owais <75408212+sidd-owais@users.noreply.github.com>
2022-09-14 17:39:45 +0530

    question_1(no of coordinate of different coordinate)

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -10,3 +10,23 @@ data = list(csv.reader(open('octant_input.csv','r')))
 
 csv.writer(open('octant_output.csv','w',newline='')).writerows(data)
 
+# Calculating the numbers of coordinate of different octant
+
+for i in range (1,len(data)):
+    line = data[i] 
+    if((float(line[7]) > 0) and (float(line[8]) > 0) and (float(line[9]) > 0)):
+        octant[1]+=1
+    if((float(line[7]) < 0) and (float(line[8]) > 0) and (float(line[9]) > 0)):
+        octant[2]+=1
+    if((float(line[7]) < 0) and (float(line[8]) < 0) and (float(line[9]) > 0)):
+        octant[3]+=1
+    if((float(line[7]) > 0) and (float(line[8]) < 0) and (float(line[9]) > 0)):
+        octant[4]+=1
+    if((float(line[7]) > 0) and (float(line[8]) > 0) and (float(line[9]) < 0)):
+        octant[5]+=1
+    if((float(line[7]) < 0) and (float(line[8]) > 0) and (float(line[9]) < 0)):
+        octant[6]+=1
+    if((float(line[7]) < 0) and (float(line[8]) < 0) and (float(line[9]) < 0)):
+        octant[7]+=1
+    if((float(line[7]) > 0) and (float(line[8]) < 0) and (float(line[9]) < 0)):
+        octant[8]+=1
