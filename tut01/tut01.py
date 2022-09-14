4c12a340f3560a9b0153fe2f1c74f9e838559e6f
sidd_owais <75408212+sidd-owais@users.noreply.github.com>
2022-09-14 17:41:37 +0530

    appending the result of question-1

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -30,3 +30,14 @@ for i in range (1,len(data)):
         octant[7]+=1
     if((float(line[7]) > 0) and (float(line[8]) < 0) and (float(line[9]) < 0)):
         octant[8]+=1
+# printing the count of diferent coordinate
+
+for i in range(1,9):
+    print(octant[i]," ",end='')
+
+print() 
+
+# Appending the octant list previously calculated
+
+data_1 = csv.writer(open('octant_output.csv','a'))
+data_1.writerow(octant)
