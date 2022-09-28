002c53526c7e140ab387bc334641749b298e0946
sidd_owais <75408212+sidd-owais@users.noreply.github.com>
2022-09-28 16:17:19 +0530

    overall octant count

--- tut02/tut02.py
+++ tut02/tut02.py
@@ -30,3 +30,41 @@ for i in range(2,len(df)+2):
 for i in range(2,len(df)+2):
     c2 = sheet.cell(i,4).value
     sheet.cell(i,10,c2-W_avg)
+# Deciding octant of different coordinate
+octant = [0,0,0,0,0,0,0,0] #List for overall count of octant
+for i in range(2,len(df)+2):
+    x = sheet.cell(i,8).value
+    y = sheet.cell(i,9).value
+    z = sheet.cell(i,10).value
+    if(x > 0 and y > 0 and z > 0):
+        sheet.cell(i,11,"+1")
+        octant[0]+=1
+    if(x < 0 and y > 0 and z > 0):
+        sheet.cell(i,11,"+2")
+        octant[2]+=1
+    if(x < 0 and y < 0 and z > 0):
+        sheet.cell(i,11,"+3")
+        octant[4]+=1
+    if(x > 0 and y < 0 and z > 0):
+        sheet.cell(i,11,"+4")
+        octant[6]+=1
+    if(x > 0 and y > 0 and z < 0):
+        sheet.cell(i,11,"-1")
+        octant[1]+=1
+    if(x < 0 and y > 0 and z < 0):
+        sheet.cell(i,11,"-2")
+        octant[3]+=1
+    if(x < 0 and y < 0 and z < 0):
+        sheet.cell(i,11,"-3")
+        octant[5]+=1
+    if(x > 0 and y < 0 and z < 0):
+        sheet.cell(i,11,"-4")
+        octant[7]+=1
+wb.save("output.xlsx")
+# Appending octant list 
+temp = [[0 for i in range(8)] for i in range(1)]
+oct = pd.DataFrame(temp, columns = ['+1','-1','+2','-2','+3','-3','+4','-4'])
+oct.iloc[0] = octant
+print(oct,"\n")
+writer = pd.ExcelWriter('output.xlsx', mode = 'a', if_sheet_exists = 'overlay')
+oct.to_excel(writer, startcol = 11 , startrow = 0, index=False)
