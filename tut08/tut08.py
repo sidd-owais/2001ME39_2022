6982ec5e6874e6c3c049cb3f768cdd9db3df9572
sidd_owais <75408212+sidd-owais@users.noreply.github.com>
2022-11-24 22:35:07 +0530

    bowling card calculation

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -103,6 +103,149 @@ def scorecard():
                             caughter + " b " + Bowler_Name
         Batting["Extras"] = ["" for i in range(6)]
         Batting["Totals"] = ["" for i in range(6)]
+    # Bowling Card
+
+        Current_baller = ""
+        count_over_run = 0
+        count_over_ball = 0
+
+        for i in range(len(l)):
+            x = re.split(r',|\!', l[i])
+            info = x[0].split(' ', 1)
+            Bats_man_Name = re.split(r'to', info[1])[1]
+            Bowler_Name = re.split(r'to', info[1])[0]
+            # SIX,FOUR,out,wides,leg byes,run,No ball(later),runout(later))
+            Event = x[1]
+            if Current_baller != Bowler_Name:
+                count_over_run = 0
+                count_over_ball = 0
+                Current_baller = Bowler_Name
+            if Bowler_Name not in Bowling.keys():
+                # (over(0),maiden(1),run(2),wicket(3),no_ball(4),wide_ball(5),economy(6))
+                Bowling[Bowler_Name] = [0 for i in range(7)]
+                Bowling_total_ball[Bowler_Name] = 0
+        # Events--------------------------------------------------------------------------------------
+            if Event.strip() == "SIX":
+                count_over_run += 1
+                count_over_ball += 1
+                Bowling[Bowler_Name][2] += 6
+                Bowling_total_ball[Bowler_Name] += 1
+                Total_Run += 6
+                Current_run += 6
+            if Event.strip() == "FOUR":
+                count_over_run += 1
+                count_over_ball += 1
+                Bowling[Bowler_Name][2] += 4
+                Bowling_total_ball[Bowler_Name] += 1
+                Total_Run += 4
+                Current_run += 4
+            if re.split(r' ', Event)[-1].strip() == "run" or re.split(r' ', Event)[-1].strip() == "runs":
+                if re.split(r' ', Event)[1].strip() == "1":
+                    Bowling[Bowler_Name][2] += 1
+                    count_over_run += 1
+                    Total_Run += 1
+                    Current_run += 1
+                if re.split(r' ', Event)[1].strip() == "2":
+                    Bowling[Bowler_Name][2] += 2
+                    count_over_run += 2
+                    Total_Run += 2
+                    Current_run += 2
+                if re.split(r' ', Event)[1].strip() == "3":
+                    Bowling[Bowler_Name][2] += 3
+                    count_over_run += 3
+                    Total_Run += 3
+                    Current_run += 3
+                count_over_ball += 1
+                Bowling_total_ball[Bowler_Name] += 1
+            if re.split(r' ', Event)[-1].strip() == "wide" or re.split(r' ', Event)[-1].strip() == "wides":
+                if re.split(r' ', Event)[-1].strip() == "wide":
+                    count_over_run += 1
+                    Bowling[Bowler_Name][2] += 1
+                    Total_Run += 1
+                    wides += 1
+                    Bowling[Bowler_Name][5] += 1
+                    Current_run += 1
+                if re.split(r' ', Event)[-1].strip() == "wides":
+                    x = int(re.split(r' ', Event)[1].strip())
+                    count_over_run += x
+                    Bowling[Bowler_Name][2] += x
+                    Total_Run += x
+                    wides += x
+                    Bowling[Bowler_Name][5] += x
+                    Current_run += x
+            if re.split(r' ', Event)[1].strip() == "out":
+                Bowling[Bowler_Name][3] += 1
+                count_over_ball += 1
+                Bowling_total_ball[Bowler_Name] += 1
+                Total_wicket += 1
+                wicket_number += 1
+                Fall_of_wicket += f'{Current_run}-{wicket_number} ({Bats_man_Name},{info[0]}),'
+            if re.split(r' ', Event)[-1].strip() == "byes":
+                Events = x[2]
+                if Events.strip() == "FOUR":
+                    count_over_ball += 1
+                    Total_Run += 4
+                    Current_run += 4
+                    if re.split(r' ', Event)[1].strip() == "leg":
+                        leg_byes += 4
+                    else:
+                        byes += 4
+                if re.split(r' ', Events)[-1].strip() == "run" or re.split(r' ', Events)[-1].strip() == "runs":
+                    if re.split(r' ', Events)[1].strip() == "1":
+                        count_over_ball += 1
+                        Total_Run += 1
+                        Current_run += 1
+                        if re.split(r' ', Event)[1].strip() == "leg":
+                            leg_byes += 1
+                        else:
+                            byes += 1
+                    if re.split(r' ', Events)[1].strip() == "2":
+                        count_over_ball += 1
+                        Total_Run += 2
+                        Current_run += 2
+                        if re.split(r' ', Event)[1].strip() == "leg":
+                            leg_byes += 2
+                        else:
+                            byes += 2
+                    if re.split(r' ', Events)[1].strip() == "3":
+                        count_over_ball += 1
+                        Total_Run += 3
+                        Current_run += 3
+                        if re.split(r' ', Event)[1].strip() == "leg":
+                            leg_byes += 3
+                        else:
+                            byes += 3
+                Bowling_total_ball[Bowler_Name] += 1
+
+            if count_over_ball == 6 and count_over_run == 0:
+                Bowling[Bowler_Name][1] += 1
+        # X---------------------------------------------------------X---------------------------X
+
+        # For Calculating Over number and Economy
+
+        for keys in Bowling.keys():
+            if keys == "BOWLER":
+                continue
+            if Bowling_total_ball[keys] % 6 == 0:
+                Bowling[keys][0] = Bowling_total_ball[keys]//6
+            else:
+                After_dot = str(Bowling_total_ball[keys] % 6)
+                Before_dot = str(Bowling_total_ball[keys]//6)
+                Bowling[keys][0] = Before_dot+"."+After_dot
+            Bowling[keys][6] = round(
+                (6*Bowling[keys][2])/Bowling_total_ball[keys], 1)
+
+        Extras = leg_byes + wides + byes
+        Extra = f'{Extras}:(b:{byes} lb:{leg_byes} w:{wides} nb:{no_ball})'
+        Batting["Extras"][1] = Extra
+        Total = f'{Total_Run} ({Total_wicket} wkts , {Total_over} Ov)'
+        Batting["Totals"][1] = Total
+
+        file_name = "scorecard.txt"
+        if count == 0:
+            if file_name not in os.listdir():
+                with open(file_name, "w") as f:
+                    f.close()
 
 
 # Code
