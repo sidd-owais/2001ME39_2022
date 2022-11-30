5833218f8df5ce75ce124700efc88e7106f64eb3
sidd_owais <75408212+sidd-owais@users.noreply.github.com>
2022-11-30 16:37:19 +0530

    solving issue of refreshing streamlit on clicking the download button

--- proj2/proj2.py
+++ proj2/proj2.py
@@ -8,10 +8,99 @@ import math
 from scipy.stats import rankdata
 import os
 from io import BytesIO
+import base64
+import os
+import json
+import pickle
+import uuid
+import re
 
 start_time = datetime.now()
 
 # Help
+
+
+def download_button(object_to_download, download_filename, button_text, pickle_it=False):
+    """
+    Generates a link to download the given object_to_download.
+
+    Params:
+    ------
+    object_to_download:  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv,
+    some_txt_output.txt download_link_text (str): Text to display for download
+    link.
+    button_text (str): Text to display on download button (e.g. 'click here to download file')
+    pickle_it (bool): If True, pickle file.
+
+    Returns:
+    -------
+    (str): the anchor tag to download object_to_download
+
+    Examples:
+    --------
+    download_link(your_df, 'YOUR_DF.csv', 'Click to download data!')
+    download_link(your_str, 'YOUR_STRING.txt', 'Click to download text!')
+
+    """
+    # if pickle_it:
+    #     try:
+    #         object_to_download = pickle.dumps(object_to_download)
+    #     except pickle.PicklingError as e:
+    #         st.write(e)
+    #         return None
+
+    # else:
+    #     if isinstance(object_to_download, bytes):
+    #         pass
+
+    #     elif isinstance(object_to_download, pd.DataFrame):
+    #         object_to_download = object_to_download.to_csv(index=False)
+
+    #     # Try JSON encode for everything else
+    #     else:
+    #         object_to_download = json.dumps(object_to_download)
+
+    try:
+        # some strings <-> bytes conversions necessary here
+        b64 = base64.b64encode(object_to_download.encode()).decode()
+
+    except AttributeError as e:
+        b64 = base64.b64encode(object_to_download).decode()
+
+    button_uuid = str(uuid.uuid4()).replace('-', '')
+    button_id = re.sub('\d+', '', button_uuid)
+
+    custom_css = f""" 
+        <style>
+            #{button_id} {{
+                background-color: rgb(255, 255, 255);
+                color: rgb(38, 39, 48);
+                padding: 0.25em 0.38em;
+                position: relative;
+                text-decoration: none;
+                border-radius: 4px;
+                border-width: 1px;
+                border-style: solid;
+                border-color: rgb(230, 234, 241);
+                border-image: initial;
+
+            }} 
+            #{button_id}:hover {{
+                border-color: rgb(246, 51, 102);
+                color: rgb(246, 51, 102);
+            }}
+            #{button_id}:active {{
+                box-shadow: none;
+                background-color: rgb(246, 51, 102);
+                color: white;
+                }}
+        </style> """
+
+    dl_link = custom_css + \
+        f'<a download="{download_filename}" id="{button_id}" href="data:file/txt;base64,{b64}">{button_text}</a><br></br>'
+
+    return dl_link
 
 
 def proj_octant_gui():
@@ -792,8 +881,9 @@ def proj_octant_gui():
             filename = file.split('.xlsx')[
                 0] + "_octant_analysis_mod_"+str(a)+".xlsx"
             st.text(filename)
-            st.download_button(label="Download File", file_name=file.split('.xlsx')[0] +
-                               "_octant_analysis_mod_"+str(a)+".xlsx", data=data_1)
+            download_button_str = download_button(data_1.getvalue(), download_filename=file.split('.xlsx')[0] +
+                                                  "_octant_analysis_mod_"+str(a)+".xlsx", button_text="Click here to download")
+            st.markdown(download_button_str, unsafe_allow_html=True)
     st.header("Project - 2")
     val = st.number_input(label="Enter Mod Value", min_value=1, step=1)
     file = st.file_uploader("Choose a File", type=["xlsx"])
